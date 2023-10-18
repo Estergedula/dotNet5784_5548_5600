c434f73aa47a8015144914d94ce0a221ef820f81
פנינה קנר <m0534147159@gmail.com>
2023-10-18 23:07:54 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program5600.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome5600();
+        Welcome5548();
+        Console.ReadKey();
+    }
+    static partial void Welcome5548();
+
+    private static void Welcome5600()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first application", name);
+    }
+}
