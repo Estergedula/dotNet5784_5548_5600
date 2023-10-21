ca8458411fac83083781f5828902ce9d311c7128
פנינה קנר <m0534147159@gmail.com>
2023-10-21 21:36:12 +0300

    21.10.23(3)

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -7,27 +7,18 @@ using System.Threading.Tasks;
 namespace DO;
 
 /// <summary>
-/// 
+/// Engineer Entity represents a student with all its prop
 /// </summary>
-/// <param name="Id"></param>
-/// <param name="Name"></param>
-/// <param name="Email"></param>
-/// <param name="Level"></param>
-/// <param name="cost"></param>
+/// <param name="Id">Personal unique ID of engineer (as in national id card)</param>
+/// <param name="Name">Name of the engineer</param>
+/// <param name="Email">o Email address of the engineer</param>
+/// <param name="Level">The level of the engineer</param>
+/// <param name="cost">Hourly cost of the engineer</param>
 public record Engineer
 (
     int Id,
     string Name,
     string? Email = null,
     EngineerExperience Level = EngineerExperience.Junior,
-    double? cost = 0
+    double? Cost = 0
 );
-{
-}
-
-{
-    /// <summary>
-    /// Date - creation date of the current student record
-    /// </summary>
-    public DateTime Date => DateTime.Now; //get only
-}
