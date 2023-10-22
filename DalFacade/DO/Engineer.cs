88ed137dd574c2d388dc99547ec0dfb333ee2ce9
פנינה קנר <m0534147159@gmail.com>
2023-10-22 11:10:10 +0300

    22.10.23M

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DO;
-
+﻿namespace DO;
 /// <summary>
 /// Engineer Entity represents a student with all its prop
 /// </summary>
 /// <param name="Id">Personal unique ID of engineer (as in national id card)</param>
 /// <param name="Name">Name of the engineer</param>
-/// <param name="Email">o Email address of the engineer</param>
+/// <param name="Email">Email address of the engineer</param>
 /// <param name="Level">The level of the engineer</param>
 /// <param name="cost">Hourly cost of the engineer</param>
 public record Engineer
@@ -20,5 +13,5 @@ public record Engineer
     string Name,
     string? Email = null,
     EngineerExperience Level = EngineerExperience.Junior,
-    double? Cost = 0
-);
+    double? Cost = 0 
+ );
