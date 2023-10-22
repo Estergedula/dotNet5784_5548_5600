90b3736539568e64996fdd924ca0c8d2a89c0ebe
פנינה קנר <m0534147159@gmail.com>
2023-10-22 11:14:16 +0300

    22.10.23M2

---
+++ DalList/EngineerImplementation.cs
@@ -0,0 +1,23 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+
+public class EngineerImplementation : IEngineer
+
+{  
+    public int Create(Engineer item)
+    { throw new NotImplementedException(); }
+
+    public Student? Read(int id)
+    { throw new NotImplementedException(); }
+
+    public List<Engineer?> ReadAll()
+    { throw new NotImplementedException(); }
+
+    public void Update(Engineer item)
+    { throw new NotImplementedException(); }
+
+    public void Delete(int id)
+    { throw new NotImplementedException(); }
+
+}
