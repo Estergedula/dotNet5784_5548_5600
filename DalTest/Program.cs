a2fadce8e599941f92ead027644723c81343e27d
פנינה קנר <m0534147159@gmail.com>
2023-10-29 18:41:40 +0200

    4

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace DalTest;
+﻿using Dal;
+using DalApi;
+using System.Runtime.CompilerServices;
+
+namespace DalTest;
 
 internal class Program
 {
+    private static IEngineer? s_dalEngineer = new EngineerImplementation();
+    private static ITask? s_dalTask = new TaskImplementation();
+    private static IDependency? s_dalDependencys = new DependencyImplementation();
+   
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Initialization.Do(s_dalEngineer,s_dalTask,s_dalDependencys);
+
     }
 }
