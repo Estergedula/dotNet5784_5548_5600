bbc7dd095b8755fa621f1c838eb3e99319b2f769
8545216@gmail.com <8545216@gmail.com>
2023-11-25 20:19:17 +0200

    25/november/2023

--- DallXml/Config.cs
+++ DallXml/Config.cs
@@ -3,8 +3,8 @@
     static internal  class Config
     {
         static string s_data_config_xml = "data-config";
-        internal static int NextTaskId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
-        internal static int nextDependencyId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "nextDependencyId"); }
+        internal static int NextTaskId { get => XmlTools.XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
+        internal static int nextDependencyId { get => XmlTools.XMLTools.GetAndIncreaseNextId(s_data_config_xml, "nextDependencyId"); }
 
     }
 }
