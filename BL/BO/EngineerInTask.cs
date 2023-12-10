94571a335d0aa15020aa578d96c134836e6783c0
פנינה קנר <m0534147159@gmail.com>
2023-12-10 03:29:10 +0200

    start interfaces

---
+++ BL/BO/EngineerInTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO;
+
+public class EngineerInTask
+{
+    public int IdOfEngineer { get; init; }
+    public int IdOfTask { get; init; }
+}
---
+++ BL/BlApi/IEngineer.cs
@@ -0,0 +1,22 @@
+﻿using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi;
+/// <summary>
+/// 
+/// </summary>
+public interface IEngineer
+{
+    //public IEnumerable<Engineer> (string nameOfProp, string value);
+    public int Create(BO.Engineer item);
+    public BO.Engineer? Read(int id);
+    public IEnumerable<BO.Engineer> ReadAll(Func<BO.Engineer?, bool> filter);
+    public void Update(BO.Engineer item);
+    public void Delete(int id);
+    public BO.TaskInList GetCurrentTaskOfEngineerDetails(int id);
+    public BO.EngineerInTask GetDetailedCourseForStudent(int StudentId, int CourseId);
+}
