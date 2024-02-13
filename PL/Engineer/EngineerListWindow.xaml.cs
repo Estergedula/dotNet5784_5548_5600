e794a244070d97585fa93a920fe88cd8376320c8
אסתר גדולה פלויט <Hand4c22@mbjcomp.org.il>
2024-02-13 19:02:02 +0200

    commit

--- PL/Engineer/EngineerListWindow.xaml.cs
+++ PL/Engineer/EngineerListWindow.xaml.cs
@@ -44,10 +44,10 @@ public partial class EngineerListWindow : Window
 
     private void cmbEngineerExperience_SelectionChange(object sender, SelectionChangedEventArgs e)
     {
+        var x = (EngineerExperience == BO.EngineerExperience.All) ? s_bl?.EngineerInList.ReadAll()! : s_bl?.EngineerInList.ReadAll(item => item!.Level == EngineerExperience)!;
         var temp = EngineerExperience == BO.EngineerExperience.All ?
         s_bl?.EngineerInList.ReadAll() :
           s_bl?.EngineerInList.ReadAll(item => item!.Level == EngineerExperience);
-        MessageBox.Show();
         EngineerList = temp == null ? new() : new(temp);
     }
 
